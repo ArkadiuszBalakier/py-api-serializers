c7a26b1f79b01078645ee99ef6df2c245f2625d3
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2026-02-09 12:39:54 +0100

    feat: add cinema urls to ciena_services, add urls for genres and actors

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,14 @@
-# write urls here
+from django.urls import path, include
+from rest_framework import routers
+
+from cinema.views import ActorViewSet, GenreViewSet
+
+app_name = 'cinema'
+
+router = routers.DefaultRouter()
+router.register(r'genres', GenreViewSet)
+router.register(r"actors", ActorViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
