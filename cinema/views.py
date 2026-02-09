ad6a088d034a6d06f73cf6406e3942954c1073a0
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2026-02-09 12:40:19 +0100

    feat: add views for Genre and Actor models

--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,18 @@
-# write views here
+from rest_framework import viewsets
+
+from cinema.models import Actor, Genre, Movie
+from cinema.serializers import ActorSerializer, GenreSerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
