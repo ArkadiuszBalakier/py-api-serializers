fee159f2d2a8a99de546f94693eb8aef7171a4fa
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2026-02-09 14:35:03 +0100

    feat: add MovieSession url

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,14 +1,15 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework import routers
 
-from cinema.views import ActorViewSet, GenreViewSet, MovieViewSet
+from cinema.views import ActorViewSet, GenreViewSet, MovieSessionViewSet, MovieViewSet
 
-app_name = 'cinema'
+app_name = "cinema"
 
 router = routers.DefaultRouter()
-router.register(r'genres', GenreViewSet)
+router.register(r"genres", GenreViewSet)
 router.register(r"actors", ActorViewSet)
-router.register(r'movies', MovieViewSet)
+router.register(r"movies", MovieViewSet)
+router.register(r"movie-sessions", MovieSessionViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
