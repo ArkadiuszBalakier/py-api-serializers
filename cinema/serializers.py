c2280cdb509f6ecbd8f210a9b0ffd3caa2a0fb73
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2026-02-09 12:41:16 +0100

    feat: add serializer for genre and actor

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,1 +1,17 @@
-# write serializers here
+from rest_framework import serializers
+
+from cinema.models import Genre, Actor
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ("id", "name")
+        read_only_fields = ("id",)
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ("id","first_name", "last_name")
+        read_only_fields = ("id",)
+
